2afb06f9d36b6216ed951a5ca96723419dbf2dcd
ddeveloper72 <duncan.niamh@gmail.com>
2018-11-28 22:35:12 +0000

    Corrected typo in models.ImageField in models.py and made makemigrations

--- posts/models.py
+++ posts/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     piblished_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
     view = models.IntegerField(default=0)
     tag = models.CharField(max_length=30, blank=True, null=True)
-    image = models=ImageField(upload_to="img", blank=True, null=True)
+    image = models.ImageField(upload_to="img", blank=True, null=True)
 
     def __unicode__(self):
         return self.title
