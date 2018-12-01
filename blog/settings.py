3e82d346027cb133a52d253a742c9fb28e3ebc54
ddeveloper72 <duncan.niamh@gmail.com>
2018-12-01 17:03:53 +0000

    Removed the hostname app url from the allowed hosts and set this in heroku config vars instead.

--- blog/settings.py
+++ blog/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     os.environ.get('C9_HOSTNAME'),
     os.environ.get('localhost', '127.0.0.1'),
-    os.environ.get('HOSTNAME', 'ddeveloper72-django-blog.herokuapp.com'),
+    os.environ.get('HOSTNAME'),
 ]
 
 
